4568a624f91a29c43b4cf7932a803ed4ac3a322a
Faye Skeen <fskeen@gmail.com>
2019-09-11 14:18:43 -0700

    Some basic helper fx set up.

---
+++ schemes/scheme-model.js
@@ -0,0 +1,65 @@
+const db = require("../data/dbConfig")
+
+module.exports = {
+    find,
+    findById,
+    add,
+    update,
+    remove,
+    getByQuery,
+    findSteps
+}
+
+// Get all schemes
+function find() {
+    return db('schemes');
+}
+
+// Get a scheme by ID
+function findById(id) {
+    return db('schemes')
+        .where({ id })
+        .first();
+}
+
+// Get steps for a given scheme
+function findSteps(id) {
+    return db('schemes')
+        .select(["amount", "schemes.id AS scheme_id", "car_id", "vin", "make"])
+        .join("cars", "cars.id", "schemes.car_id")
+        .where({ "schemes.id": id })
+        .first();
+}
+
+// Post a scheme to the db
+function add(scheme) {
+    return db('schemes')
+        .insert(scheme)
+        .then(id => {
+            return getById(id[0])
+        });
+}
+
+// Edit a scheme in the db
+function update(id, changes) {
+    return db('schemes')
+        .where('id', id)
+        .update(changes)
+        .then(id => {
+            return getById(id[0])
+        });;
+}
+
+// Delete a scheme in the db
+function remove(id) {
+    return db('schemes')
+        .where('id', id)
+        .del();
+}
+
+// Get scheme list by specific params
+function getByQuery(limit, sortBy, sortDir) {
+    return db('schemes')
+        .limit(limit)
+        .orderBy(sortBy, sortDir)
+}
